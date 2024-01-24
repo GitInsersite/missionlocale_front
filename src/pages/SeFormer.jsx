70493c48df2adb7a31af5d7e8e20a313391ccad9
unknown <steevoyini@yahoo.fr>
2024-01-24 15:19:01 +0100

    nouveau design en cours de r

--- src/pages/SeFormer.jsx
+++ src/pages/SeFormer.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
 
 function SeFormer() {
   return (
-    <div>
+    <div className="bg-[#F6F6F6]">
       <div
         className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
         style={{ backgroundImage: "url(/public/MicrosoftTeams-image12.png)" }}
@@ -10,81 +11,89 @@ function SeFormer() {
         <h1 className="md:text-3xl">SE FORMER</h1>
       </div>
 
-      <div className="flex flex-col items-center">
-        <div>   
+      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+        <div className="flex-row justify-content px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
+          <div className="flex items-center">Accueil <IoIosArrowForward /> Services <IoIosArrowForward /><span className="text-[#D70B52]" aria-current="page">Se former</span></div>
+        </div>
+        <br />
+        <br />
+        <div >
           <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
 
-      <div class="flex font-sans">
-  <div class="flex-none w-56 relative">
-    <img src="/" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
-  </div>
+            <div class="flex font-sans">
+              <div class="flex-none w-56 relative">
+                <img src="/women.jpg" alt="" className="absolute inset-0 w-full h-full object-cover rounded-[20%]" loading="lazy" />
+              </div>
 
-  <div>
-  <form class="flex-auto p-6">
-    <div class="flex flex-wrap">
-      
-    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-              <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
-                FORMA
-              </span>
-              TION CONTINUE...
-            </h2>
-            <p class="text-sm text-slate-500">
-              Une formation pré-qualifiante est, pourrait-on dire, une
-              introduction, une découverte d’un métier ou d’un secteur
-              professionnel. Durant celle-ci, vous seront présentées les bases
-              constitutives, les pratiques courantes, pour y évoluer. Vous
-              effectuerez aussi des stages qui vous permettront d’avoir une
-              première expérience pratique dans le domaine choisi.{" "}
-            </p>
+              <div className="bg-white rounded-[10%] ">
+                <form class="flex-auto p-6">
+                  <div class="flex flex-wrap t-20px l-20px relative">
 
-    </div>
-  </form>    
-</div>
+                    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                      <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
+                        FORMA
+                      </span>
+                      TION CONTINUE...
+                    </h2>
+                    <p class="text-sm text-slate-500">
+                      Une formation pré-qualifiante est, pourrait-on dire, une
+                      introduction, une découverte d’un métier ou d’un secteur
+                      professionnel. Durant celle-ci, vous seront présentées les bases
+                      constitutives, les pratiques courantes, pour y évoluer. Vous
+                      effectuerez aussi des stages qui vous permettront d’avoir une
+                      première expérience pratique dans le domaine choisi.{" "}
+                    </p>
+
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br />
+
+        <div className="flex flex-col">
+          <div >
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
+              <div class="flex font-sans ">
+
+                <form class="flex-auto p-6 bg-white rounded-[10%]">
+                  <div class="flex flex-wrap ">
+
+                    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                      <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
+                        FORMA
+                      </span>
+                      TION EN ALTERNANCE...
+                    </h2>
+                    <p class="text-sm text-slate-500">
+                      Les formations qualifiantes sont des formations de durée variable,
+                      mais longues pour la plupart, préparant à une qualification reconnue,
+                      que ce soit par le biais de la préparation à un diplôme ou encore à un
+                      titre professionnel. Elles se composent de cours théoriques et de
+                      stages.{" "}
+                    </p>
+                    <div className="flex flex-col px-4 pt-4 font-semibold justify:center md:px-14 lg:px-20">
+                      <Link to="/formations" onClick={() => window.scrollTo(0, 0)} className="bg-[#D70B52] text-white font-semibold py-1 text-center rounded mb-8 text-sm md:w-56">
+                        OFFRES DE FORMATIONS
+                      </Link>
+                    </div>
+
+                  </div>
+                </form>
+                <div class="flex-none w-56 relative">
+                  <img src="/travailleur.jpg" alt="" className="absolute inset-0 w-full h-full object-cover rounded-[20%]" loading="lazy" />
+                </div>
+              </div>
+            </div>
+
+            <br />
+            <br />
           </div>
         </div>
       </div>
-
-
-      <div className="flex flex-col items-center">
-        <div>   
-          <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
-      <div class="flex font-sans">
-  
-  <form class="flex-auto p-6">
-    <div class="flex flex-wrap">
-      
-    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-              <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
-                FORMA
-              </span>
-              TION EN ALTERNANCE...
-            </h2>
-            <p class="text-sm text-slate-500">
-            Les formations qualifiantes sont des formations de durée variable,
-          mais longues pour la plupart, préparant à une qualification reconnue,
-          que ce soit par le biais de la préparation à un diplôme ou encore à un
-          titre professionnel. Elles se composent de cours théoriques et de
-          stages.{" "}
-          </p>
-          <div className="flex flex-col px-4 pt-4 font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
-        <Link to="/formations" onClick={() => window.scrollTo(0, 0)} className="bg-[#D70B52] text-white font-semibold py-1 text-center rounded-lg mb-8 text-sm md:w-56">
-          OFFRES DE FORMATIONS
-        </Link>
-      </div>
-            
-    </div>  
-  </form>
-  <div class="flex-none w-56 relative">
-    <img src="/smileyrousse.jpg" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
     </div>
-  </div>    
-</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
   );
 }
 
--- src/pages/Sorienter.jsx
+++ src/pages/Sorienter.jsx
@@ -35,8 +35,14 @@ function Sorienter() {
       >
         <h1 className="md:text-3xl">S&apos;ORIENTER</h1>
       </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+
+
+    <div className="flex px-4 pt-4 font-semibold md:px-14 lg:px-20">
+      <figure className="md:flex bg-white rounded-[20%] mx-auto p-8">
+        <img class=" rounded-[20%]" src="\jeunefemme.jpg" alt="" width="384" height="51"/>
+        <div className="transform translate-y-11 pt-6 space-y-4">
+          <blockquote>
+          <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
           <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
             Quel mét
           </span>
@@ -55,79 +61,11 @@ function Sorienter() {
           découvrir les métiers et voies professionnelles possibles, en fonction
           de vos compétences et aspirations.
         </p>
-      </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
-            Les pla
-          </span>
-          teaux techniques
-        </h2>
-        <p className="mb-6">
-          Découvrez un secteur professionnel par une mise en situation réelle de
-          travail (PMSMP), et apprenez-en pus sur le travail en équipe, les
-          conditions de travail, ainsi que sur les premiers gestes techniques.
-          Pendant 2 semaines, tout jeune sans projet professionnel ou souhaitant
-          valider un projet découvrira plusieurs métiers grâce à de la pratique,
-          des mises en situation, qui définiront alors plus précisément ses
-          objectifs d’orientation.
-        </p>
-      </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D60B52] pb-[0.5px]">E2</span>C
-        </h2>
-        <p className="mb-6">
-          Un parcours qui vous propose un programme personnalisé pour accéder à
-          l’emploi, reposant sur l’amélioration des savoirs de base (français,
-          mathématiques, informatique) et le développement de compétences
-          professionnelles, sociales et personnelles. L’E2C propose un parcours
-          de formation en alternance de 10 et mois et demi maximum, ainsi que
-          des stages en entreprises pour découvrir des métiers et construire
-          ainsi votre projet professionnel. La formation est gratuite et
-          indemnisée après une validation de période d’essai d’un mois. Pour
-          entreprendre ce parcours, référez-vous en à votre conseiller de la
-          Mission Locale.
-        </p>
-      </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D60B52] pb-[0.5px]">Le P</span>
-          EE
-        </h2>
-        <p className="mb-6">
-          Un dispositif régional pour vous aider à construire et/ou confirmer
-          votre projet professionnel et/ou à développer des compétences de base
-          en situation professionnelle. Des stages en entreprise, des plateaux
-          techniques et des stages dans le cadre des passerelles de découverte
-          de l’apprentissage… sont autant d’options disponibles dans ce
-          dispositif immersif pour accéder à une formation qualifiante ou à un
-          emploi direct, grâce à l’expérience acquise lors de ces stages.
-        </p>
-        <p className="mb-6">
-          La durée moyenne de ce parcours gratuit est de 736 heures en centre
-          auxquelles s’ajoutent 4 semaines en entreprise. Vous bénéficiez alors
-          du statut de stagiaire de la formation professionnelle ainsi que d’une
-          protection sociale.{" "}
-        </p>
-      </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D60B52] pb-[0.5px]">L’EDI </span>
-          (Espace Dynamique d’Insertion)
-        </h2>
-        <p className="mb-6">
-          Un parcours qui vous propose un programme personnalisé pour accéder à
-          l’emploi, reposant sur l’amélioration des savoirs de base (français,
-          mathématiques, informatique) et le développement de compétences
-          professionnelles, sociales et personnelles. L’E2C propose un parcours
-          de formation en alternance de 10 et mois et demi maximum, ainsi que
-          des stages en entreprises pour découvrir des métiers et construire
-          ainsi votre projet professionnel. La formation est gratuite et
-          indemnisée après une validation de période d’essai d’un mois. Pour
-          entreprendre ce parcours, référez-vous en à votre conseiller de la
-          Mission Locale.
-        </p>
+          </blockquote>
+        <figcaption class="text justify center ">
+    
+          <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+        
         {error && <div className="text-red-500">{error}</div>}{" "}
         {/* Display error message */}
         {success && <div className="text-green-500">{success}</div>}{" "}
@@ -139,6 +77,13 @@ function Sorienter() {
           PRENDRE RDV AVEC UN CONSEILLER
         </button>
       </div>
+        </figcaption>
+        </div>
+      </figure>
+      </div>
+
+      <br />
+      
     </div>
   );
 }
