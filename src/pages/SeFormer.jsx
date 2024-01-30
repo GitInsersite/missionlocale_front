bccea5bb15837eb334fb18792d76455663ad6705
Kibouu945 <rokibtadjou17@gmail.com>
2024-01-30 14:53:20 +0100

    Updates de certaines pages niveau design

--- src/pages/SeFormer.jsx
+++ src/pages/SeFormer.jsx
@@ -1,92 +1,114 @@
 import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
 
 function SeFormer() {
   return (
-    <div>
+    <div className="bg-[#F6F6F6]">
       <div
         className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
         style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
       >
         <h1 className="md:text-3xl">SE FORMER</h1>
       </div>
-      <div className="flex flex-col items-center">
-        <div>
-          <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-            <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-              <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
-                Aide à
-              </span>{" "}
-              l’accès à la formation
-            </h2>
-            <h3 className="leading-tight">
-              La Mission Locale des Mureaux vous soutient et vous accompagne
-              dans votre projet de qualification professionnelle :{" "}
-            </h3>
-            <ul className="mb-6 mt-6 leading-tight">
-              <li>
-                - conseils sur les formations et les financements possibles{" "}
-              </li>
-              <li>
-                - recherche de l’organisme de formation le plus adapté à votre
-                situation{" "}
-              </li>
-              <li>- mise en relation avec l’organisme de formations </li>
-              <li>- préparation à l’entrée en formations </li>
-              <li>- soutien tout au long du parcours de formations </li>
-            </ul>
-            <p className="mb-6 leading-tight">
-              Pour accéder à l’une de nos formations proposées, contactez votre
-              conseiller de la Mission Locale.
-            </p>
-          </div>
-          <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
-            <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-              <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
-                Les for
-              </span>
-              mations pré-qualifiantes
-            </h2>
-            <p className="mb-6">
-              Une formation pré-qualifiante est, pourrait-on dire, une
-              introduction, une découverte d’un métier ou d’un secteur
-              professionnel. Durant celle-ci, vous seront présentées les bases
-              constitutives, les pratiques courantes, pour y évoluer. Vous
-              effectuerez aussi des stages qui vous permettront d’avoir une
-              première expérience pratique dans le domaine choisi.{" "}
-            </p>
+
+      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-40">
+        <div className="flex-row justify-content px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
+          <div className="flex items-center">
+            Accueil <IoIosArrowForward /> Services <IoIosArrowForward />
+            <span className="text-[#D70B52]" aria-current="page">
+              Se former
+            </span>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col px-4 pt-4  font-semibold md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
-            Les for
-          </span>
-          mations qualifiantes
-        </h2>
-        <p className="mb-6">
-          Les formations qualifiantes sont des formations de durée variable,
-          mais longues pour la plupart, préparant à une qualification reconnue,
-          que ce soit par le biais de la préparation à un diplôme ou encore à un
-          titre professionnel. Elles se composent de cours théoriques et de
-          stages.
-        </p>
-      </div>
-      <div className="flex flex-col px-4 pt-4 font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
-        <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D70B52] pb-[0.5px]">Appr</span>
-          entissage
-        </h2>
-        <p className="mb-6">
-          Les formations qualifiantes sont des formations de durée variable,
-          mais longues pour la plupart, préparant à une qualification reconnue,
-          que ce soit par le biais de la préparation à un diplôme ou encore à un
-          titre professionnel. Elles se composent de cours théoriques et de
-          stages.
-        </p>
-        <Link to="/formations" onClick={() => window.scrollTo(0, 0)} className="bg-[#D70B52] text-white font-semibold py-1 text-center rounded-lg mb-8 text-sm md:w-56">
-          OFFRES DE FORMATIONS
-        </Link>
+        <br />
+        <div>
+          <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
+            <div className="flex font-sans">
+              <div className="flex-none w-56 relative">
+                <img
+                  src="/women.jpg"
+                  alt=""
+                  className="absolute inset-0 w-full h-full object-cover rounded-[20%]"
+                  loading="lazy"
+                />
+              </div>
+
+              <div className="bg-white rounded-[5%] ">
+                <form class="flex-auto p-6">
+                  <div className="flex flex-wrap t-20px l-20px relative">
+                    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                      <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
+                        FORMA
+                      </span>
+                      TION CONTINUE...
+                    </h2>
+                    <p class="text-sm text-slate-500">
+                      La Mission Locale des Mureaux te soutient et t’accompagne
+                      dans ton projet de qualification professionnelle, avec des
+                      conseils sur les formations et les financements possibles,
+                      de la recherche sur l’organisme de formation le plus
+                      adapté à ta situation, la mise en relation avec
+                      l’organisme, la préparation à l’entrée en formation et le
+                      soutien tout au long du parcours. Nous proposons parmi
+                      notre panel de formation des formations qualifiantes,
+                      pré-qualifiantes, ainsi que des remises à niveau.{" "}
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br /> <br /> <br />
+        <div className="flex flex-col">
+          <div>
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20">
+              <div className="flex font-sans ">
+                <form class="flex-auto p-6 bg-white rounded-[5%]">
+                  <div className="flex flex-wrap">
+                    <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                      <span className="border-b-2 border-[#D70B52] pb-[0.5px]">
+                        FORMA
+                      </span>
+                      TION EN ALTERNANCE...
+                    </h2>
+                    <p class="text-sm text-slate-500">
+                      Pour ce qui est de l’alternance, des ateliers sont
+                      proposés afin de présenter le contrat d’apprentissage ou
+                      de professionnalisation. Avec ces ateliers, informe-toi
+                      sur les CFA correspondant aux formations choisies, les
+                      droits et les devoirs des apprentis, la rémunération ainsi
+                      que sur les aides financières aux entreprises. Découvre
+                      aussi nos Prépa apprentissage, qui t’offrent un
+                      accompagnement complet pour sécuriser ton entrée en
+                      contrat d’apprentissage.{" "}
+                    </p>
+                    <div className="flex flex-col px-4 pt-4 font-semibold justify:center md:px-14 lg:px-20">
+                      <Link
+                        to="/formations"
+                        onClick={() => window.scrollTo(0, 0)}
+                        className="bg-[#D70B52] text-white font-semibold py-1 text-center rounded mb-8 text-sm md:w-56"
+                      >
+                        OFFRES DE FORMATIONS
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+                <div className="flex-none w-56 relative">
+                  <img
+                    src="/travailleur.jpg"
+                    alt=""
+                    className="absolute inset-0 w-full h-full object-cover rounded-[20%]"
+                    loading="lazy"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <br />
+            <br />
+          </div>
+        </div>
       </div>
     </div>
   );
