21b872212038f81c73208eb37bc2d6cb40d11abb
Kibouu945 <rokibtadjou17@gmail.com>
2024-01-30 16:12:44 +0100

    Update EtreAccompagné

--- src/pages/EtreAccompagne.jsx
+++ src/pages/EtreAccompagne.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
-import '../index.css';
+import "../index.css";
 
 function EtreAccompagne() {
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
   // Dynamically get the API URL based on the environment
-  const apiUrlEnv = import.meta.env.MODE === 'production'
-  ? import.meta.env.VITE_API_URL_PROD
-  : import.meta.env.VITE_API_URL_DEV;
+  const apiUrlEnv =
+    import.meta.env.MODE === "production"
+      ? import.meta.env.VITE_API_URL_PROD
+      : import.meta.env.VITE_API_URL_DEV;
 
   const handleInscription = () => {
     // Make API call to register for the workshop
-    const registrationUrl =
-      `${apiUrlEnv}/api/notifierConseillerFormulaire`;
+    const registrationUrl = `${apiUrlEnv}/api/notifierConseillerFormulaire`;
 
     axios
       .post(registrationUrl)
@@ -40,41 +40,32 @@ function EtreAccompagne() {
         style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
       >
         <h1 className="md:text-3xl">ETRE ACCOMPAGNE</h1>
-      </div> <br /> <br />
-
-
+      </div>{" "}
+      <br /> <br />
       <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
-        <a href="/">Accueil</a> {'>'}
-        <a href="/Services"> Services</a> {'>'}
-        <a className="text-[#D70B52]" href="/Etreaccompagne"> Etre accompagné</a>
-      </div> <br />
-
-      <section>
-        <div className="container">
-          <div className="carte" style="--clr:#ff0066;">
-            <div className="imgBx">
-              <img src="/ouvriers.jpg" />
-            </div>
-            <div className="conten">
-              <h2> UN ACCOMPAGNEMENT PERSONNALISE</h2>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis consectetur eligendi sapiente blanditiis harum ex laboriosam tempora porro. Consectetur ratione quasi velit ipsam natus impedit necessitatibus iusto quaerat culpa ut!</p>
-              <a href="#">PRENDRE RDV AVEC UN CONSEILLER</a>
-            </div>
-          </div>
-        </div>
-      </section>
-
+        <a href="/">Accueil</a> {">"}
+        <a href="/Services"> Services</a> {">"}
+        <a className="text-[#D70B52]" href="/Etreaccompagne">
+          {" "}
+          Etre accompagné
+        </a>
+      </div>{" "}
+      <br />
       <div>
         <div className="flex flex-col pl-4 p-4 px-4 pt-4 font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
           <div className="flex font-sans">
             <div className="flex-none w-56">
-              <img src="/ouvriers.jpg" alt="" className="object-cover rounded-[20%]" loading="lazy" />
+              <img
+                src="/ouvriers.jpg"
+                alt=""
+                className="object-cover rounded-[20%]"
+                loading="lazy"
+              />
             </div>
 
             <div className="bg-white rounded-[5%] ">
               <form className="flex-auto p-6">
                 <div className="flex flex-wrap t-20px l-20px relative">
-
                   <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
                     <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
                       UN ACCOM
@@ -82,29 +73,42 @@ function EtreAccompagne() {
                     PAGNEMENT PERSONNALISE
                   </h2>
                   <p className="text-sm text-slate-500">
-                    Un accompagnement vous sera proposé pour vous aider dans votre
-                    recherche d’emploi ou de formation, personnalisé en fonction de vos
-                    attentes. Ces accompagnements seront aussi là pour vous aider sur le
-                    plan social, tels que la santé, le logement ou bien encore la
-                    mobilité. Un conseiller vous est assigné pour vous accompagner et vous
-                    aider dans toutes les démarches d’insertion, d’orientation, de
-                    formation ou de recherche d’emploi.
+                    Un accompagnement est proposé pour t’aider dans ta recherche
+                    d’emploi ou de formation, personnalisé en fonction de tes
+                    attentes. Ces accompagnements seront aussi là pour t’aider
+                    sur le plan social, tels que la santé, le logement ou bien
+                    encore la mobilité.
                   </p>
+                  <br />
+
+                  <p className="text-sm text-slate-500">
+                    Un conseiller est assigné pour t’accompagner et t’aider dans
+                    toutes les démarches d’insertion, d’orientation, de
+                    formation ou de recherche d’emploi. De nombreux dispositifs
+                    te seront alors présentés pour t’aider dans tes démarches
+                    professionnelles (PACEA, CEJ, Parrainage...) et sociales
+                    (Santé, Logement, Mobilité).
+                  </p>
+
                   <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
                     {error && <div className="text-red-500">{error}</div>}{" "}
                     {/* Display error message */}
-                    {success && <div className="text-green-500">{success}</div>}{" "}
+                    {success && (
+                      <div className="text-green-500">{success}</div>
+                    )}{" "}
                     {/* Display success message */}
-                    <button onClick={handleInscription} className="bg-[#D60B52] text-white font-semibold py-2 text-center rounded-lg text-sm mb-6 md:w-64">
+                    <button
+                      onClick={handleInscription}
+                      className="bg-[#D60B52] text-white font-semibold py-2 text-center rounded-lg text-sm mb-6 md:w-64"
+                    >
                       PRENDRE RDV AVEC UN CONSEILLER
                     </button>
                   </div>
                 </div>
-
               </form>
             </div>
-          </div> <br /> < br />
-
+          </div>{" "}
+          <br /> <br />
         </div>
       </div>
     </div>
