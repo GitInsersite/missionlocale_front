594ba81456b19f3da29ae1ffc919e9fb8e070f50
Aldo Miralles <alditomiralles3@gmail.com>
2024-02-08 23:02:22 +0100

    Updating EtreAccompagne, Sorienter, TrouverUnEmploi version mobile

--- src/pages/Sorienter.jsx
+++ src/pages/Sorienter.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+import { useMediaQuery } from "react-responsive";
 
 function Sorienter() {
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+
+  const isTabletOrLarger = useMediaQuery({ minWidth: 768 });
 
   // Dynamically get the API URL based on the environment
   const apiUrlEnv =
@@ -33,87 +36,173 @@ function Sorienter() {
   };
 
   return (
-    <div className="bg-[#F6F6F6]">
-      <div
-        className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
-        style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
-      >
-        <h1 className="md:text-3xl">S&apos;ORIENTER</h1>
-      </div>{" "}
-      <br />
-      <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
-        <a href="/">Accueil</a> {">"}
-        <a href="/Services"> Services </a>
-        {">"}
-        <a className="text-[#D70B52]" href="/Sorientier">
-          {" "}
-          S'orientier
-        </a>
-      </div>{" "}
-      <br />
-      <div>
-        <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
-          <div className="flex font-sans">
-            <div className="flex-none w-56">
-              <img
-                className="object-cover rounded-[20%]"
-                loading="lazy"
-                src="\jeunefemme.jpg"
-                alt=""
-              />
-            </div>
-
-            <div className="bg-white rounded-[5%] flex font-sans">
-              <form class="flex-auto p-6 bg-white rounded-[5%]">
-                <div className="flex flex-wrap">
-                  <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-                    <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
-                      QUEL MET
-                    </span>
-                    IER EST FAIT POUR MOI ?
-                  </h2>
-                  <p class="text-sm text-slate-500">
-                    Au sein de la Mission Locale, nous t’informons sur toutes
-                    les possibilités de métiers et de formations
-                    professionnelles. En fonction de tes centres d’intérêt, un
-                    grand nombre de possibilités s’ouvrent à toi. La Mission
-                    Locale te donne la possibilité de découvrir les métiers et
-                    voies professionnelles possibles, en fonction de tes
-                    compétences et aspirations. Cela passe par différents
-                    parcours personnalisés, ateliers, et mises en situation
-                    réelles de travail pour vous aider à trouver votre voie dans
-                    le monde professionnel.
-                  </p>{" "}
-                  <br />
-                  <p class="text-sm text-slate-500">
-                    En fonction de vos centres d’intérêt, un grand nombre de
-                    possibilités s’ouvrent à vous. La Mission Locale vous donne
-                    la possibilité de découvrir les métiers et voies
-                    professionnelles possibles, en fonction de vos compétences
-                    et aspirations.
-                  </p>
-                  <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-                    {error && <div className="text-red-500">{error}</div>}{" "}
-                    {/* Display error message */}
-                    {success && (
-                      <div className="text-green-500">{success}</div>
-                    )}{" "}
-                    {/* Display success message */}
-                    <button
-                      onClick={handleInscription}
-                      className="bg-[#D60B52] text-white font-semibold py-1 text-center rounded-lg text-sm mb-6 md:w-64"
-                    >
-                      PRENDRE RDV AVEC UN CONSEILLER
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+    <>
+      {isTabletOrLarger ? (
+        <div className="bg-[#F6F6F6]">
+          <div
+            className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
+            style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
+          >
+            <h1 className="md:text-3xl">S&apos;ORIENTER</h1>
           </div>{" "}
-          <br /> <br />
-        </div>{" "}
-      </div>
-    </div>
+          <br />
+          <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
+            <a href="/">Accueil</a> {">"}
+            <a href="/Services"> Services </a>
+            {">"}
+            <a className="text-[#D70B52]" href="/Sorientier">
+              {" "}
+              S&apos;orientier
+            </a>
+          </div>{" "}
+          <br />
+          <div>
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
+              <div className="flex font-sans">
+                <div className="flex-none w-56">
+                  <img
+                    className="object-cover rounded-[20%]"
+                    loading="lazy"
+                    src="\jeunefemme.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="bg-white rounded-[5%] flex font-sans">
+                  <form className="flex-auto p-6 bg-white rounded-[5%]">
+                    <div className="flex flex-wrap">
+                      <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                        <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
+                          QUEL MET
+                        </span>
+                        IER EST FAIT POUR MOI ?
+                      </h2>
+                      <p className="text-sm text-slate-500">
+                        Au sein de la Mission Locale, nous t’informons sur
+                        toutes les possibilités de métiers et de formations
+                        professionnelles. En fonction de tes centres d’intérêt,
+                        un grand nombre de possibilités s’ouvrent à toi. La
+                        Mission Locale te donne la possibilité de découvrir les
+                        métiers et voies professionnelles possibles, en fonction
+                        de tes compétences et aspirations. Cela passe par
+                        différents parcours personnalisés, ateliers, et mises en
+                        situation réelles de travail pour vous aider à trouver
+                        votre voie dans le monde professionnel.
+                      </p>{" "}
+                      <br />
+                      <p className="text-sm text-slate-500">
+                        En fonction de vos centres d’intérêt, un grand nombre de
+                        possibilités s’ouvrent à vous. La Mission Locale vous
+                        donne la possibilité de découvrir les métiers et voies
+                        professionnelles possibles, en fonction de vos
+                        compétences et aspirations.
+                      </p>
+                      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+                        {error && <div className="text-red-500">{error}</div>}{" "}
+                        {/* Display error message */}
+                        {success && (
+                          <div className="text-green-500">{success}</div>
+                        )}{" "}
+                        {/* Display success message */}
+                        <button
+                          onClick={handleInscription}
+                          className="bg-[#D60B52] text-white font-semibold py-1 text-center rounded-lg text-sm mb-6 md:w-64"
+                        >
+                          PRENDRE RDV AVEC UN CONSEILLER
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>{" "}
+              <br /> <br />
+            </div>{" "}
+          </div>
+        </div>
+      ) : (
+        <div className="bg-[#F6F6F6]">
+          <div
+            className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
+            style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
+          >
+            <h1 className="md:text-3xl">S&apos;ORIENTER</h1>
+          </div>{" "}
+          <br />
+          <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
+            <a href="/">Accueil</a> {">"}
+            <a href="/Services"> Services </a>
+            {">"}
+            <a className="text-[#D70B52]" href="/Sorientier">
+              {" "}
+              S&apos;orientier
+            </a>
+          </div>{" "}
+          <br />
+          <div>
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
+              <div className="flex-col font-sans">
+                <div className="flex-none w-full">
+                  <img
+                    className="object-cover"
+                    loading="lazy"
+                    src="\jeunefemme.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="bg-white flex font-sans">
+                  <form className="flex-auto p-6 bg-white rounded-[5%]">
+                    <div className="flex flex-wrap">
+                      <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                        <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
+                          QUEL MET
+                        </span>
+                        IER EST FAIT POUR MOI ?
+                      </h2>
+                      <p className="text-sm text-slate-500">
+                        Au sein de la Mission Locale, nous t’informons sur
+                        toutes les possibilités de métiers et de formations
+                        professionnelles. En fonction de tes centres d’intérêt,
+                        un grand nombre de possibilités s’ouvrent à toi. La
+                        Mission Locale te donne la possibilité de découvrir les
+                        métiers et voies professionnelles possibles, en fonction
+                        de tes compétences et aspirations. Cela passe par
+                        différents parcours personnalisés, ateliers, et mises en
+                        situation réelles de travail pour vous aider à trouver
+                        votre voie dans le monde professionnel.
+                      </p>{" "}
+                      <br />
+                      <p className="text-sm text-slate-500">
+                        En fonction de vos centres d’intérêt, un grand nombre de
+                        possibilités s’ouvrent à vous. La Mission Locale vous
+                        donne la possibilité de découvrir les métiers et voies
+                        professionnelles possibles, en fonction de vos
+                        compétences et aspirations.
+                      </p>
+                      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+                        {error && <div className="text-red-500">{error}</div>}{" "}
+                        {/* Display error message */}
+                        {success && (
+                          <div className="text-green-500">{success}</div>
+                        )}{" "}
+                        {/* Display success message */}
+                        <button
+                          onClick={handleInscription}
+                          className="bg-[#D60B52] text-white font-semibold py-1 text-center rounded-lg text-sm mb-6 md:w-64"
+                        >
+                          PRENDRE RDV AVEC UN CONSEILLER
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>{" "}
+              <br /> <br />
+            </div>{" "}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/TrouverUnEmploi.jsx
+++ src/pages/TrouverUnEmploi.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import { useMediaQuery } from "react-responsive";
 
 function TrouverUnEmploi() {
-
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
+  const isTabletOrLarger = useMediaQuery({ minWidth: 768 });
+
   // Dynamically get the API URL based on the environment
-  const apiUrlEnv = import.meta.env.MODE === 'production'
-  ? import.meta.env.VITE_API_URL_PROD
-  : import.meta.env.VITE_API_URL_DEV;
+  const apiUrlEnv =
+    import.meta.env.MODE === "production"
+      ? import.meta.env.VITE_API_URL_PROD
+      : import.meta.env.VITE_API_URL_DEV;
 
   const handleInscription = () => {
     // Retrieve the token from local storage
@@ -52,68 +55,163 @@ function TrouverUnEmploi() {
   };
 
   return (
-    <div className="bg-[#F6F6F6]">
-      <div
-        className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
-        style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
-      >
-        <h1 className="md:text-3xl">TROUVER UN EMPLOI</h1>
-      </div>
-
-      <br /> <br />
-      <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
-       <a href="/">Accueil</a>{'>'}
-        <a href="/Services">Services</a> {'>'}
-        <a className="text-[#D70B52]" href="/TrouverUnEmploi">Trouver un emploi</a>
-        </div> <br />
-
-      <div>
-      <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
-
-            <div className="flex font-sans">
-              <div className="flex-none w-56">
-                <img src="/smileyrousse.jpg" alt="" className="object-cover rounded-[20%]" loading="lazy" />
-              </div>
-
-              <div className="bg-white rounded-[5%] ">
-                <form class="flex-auto p-6">
-                  <div className="flex flex-wrap t-20px l-20px relative">
-
-                  <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
-          <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
-            LA MISSION
-          </span>{" "}
-          LOCALE T’ACCOMPAGNE VERS L’EMPLOI
-        </h2>
-        <p className="text-sm text-slate-500">
-        Des équipes spécialisées en lien avec ton conseiller sont là pour t’aider à trouver une emploi ! 
-        Elles sont là pour négocier des offres d’emploi avec les entreprises, 
-        organiser des actions de recrutement ainsi que te préparer activement à la recherche d’emploi et à la rencontre avec les employeurs. 
-        </p>
-        <br />
-        <p className="text-sm text-slate-500">
-        N’hésite donc pas à prendre rendez-vous avec ton conseiller si tu es à la recherche d’un emploi et pour que 
-        l’on puisse te présenter nos conseils et nos différents dispositifs d’insertion professionnelle. 
-        </p>
-        <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
-        {error && <div className="text-red-500">{error}</div>}{" "}
-        {/* Display error message */}
-        {success && <div className="text-green-500">{success}</div>}{" "}
-        {/* Display success message */}
-        <button 
-        onClick={handleInscription}
-        className="bg-[#D60B52] text-white font-semibold py-2 px-2 text-center rounded-lg text-sm mb-6 md:w-64">
-          PRENDRE RDV AVEC UN CONSEILLER
-        </button> 
+    <>
+      {isTabletOrLarger ? (
+        <div className="bg-[#F6F6F6]">
+          <div
+            className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
+            style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
+          >
+            <h1 className="md:text-3xl">TROUVER UN EMPLOI</h1>
           </div>
+          <br /> <br />
+          <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
+            <a href="/">Accueil</a>
+            {">"}
+            <a href="/Services">Services</a> {">"}
+            <a className="text-[#D70B52]" href="/TrouverUnEmploi">
+              Trouver un emploi
+            </a>
+          </div>{" "}
+          <br />
+          <div>
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
+              <div className="flex font-sans">
+                <div className="flex-none w-56">
+                  <img
+                    src="/smileyrousse.jpg"
+                    alt=""
+                    className="object-cover rounded-[20%]"
+                    loading="lazy"
+                  />
+                </div>
+
+                <div className="bg-white rounded-[5%] ">
+                  <form className="flex-auto p-6">
+                    <div className="flex flex-wrap t-20px l-20px relative">
+                      <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                        <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
+                          LA MISSION
+                        </span>{" "}
+                        LOCALE T’ACCOMPAGNE VERS L’EMPLOI
+                      </h2>
+                      <p className="text-sm text-slate-500">
+                        Des équipes spécialisées en lien avec ton conseiller
+                        sont là pour t’aider à trouver une emploi ! Elles sont
+                        là pour négocier des offres d’emploi avec les
+                        entreprises, organiser des actions de recrutement ainsi
+                        que te préparer activement à la recherche d’emploi et à
+                        la rencontre avec les employeurs.
+                      </p>
+                      <br />
+                      <p className="text-sm text-slate-500">
+                        N’hésite donc pas à prendre rendez-vous avec ton
+                        conseiller si tu es à la recherche d’un emploi et pour
+                        que l’on puisse te présenter nos conseils et nos
+                        différents dispositifs d’insertion professionnelle.
+                      </p>
+                      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+                        {error && <div className="text-red-500">{error}</div>}{" "}
+                        {/* Display error message */}
+                        {success && (
+                          <div className="text-green-500">{success}</div>
+                        )}{" "}
+                        {/* Display success message */}
+                        <button
+                          onClick={handleInscription}
+                          className="bg-[#D60B52] text-white font-semibold py-2 px-2 text-center rounded-lg text-sm mb-6 md:w-64"
+                        >
+                          PRENDRE RDV AVEC UN CONSEILLER
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>{" "}
+            <br />
+          </div>{" "}
+          <br />
         </div>
-        </form>
+      ) : (
+        <div className="bg-[#F6F6F6]">
+          <div
+            className="bg-image bg-cover bg-center h-12 flex justify-center items-center text-white font-bold sm:h-16 md:h-28 lg:h-40 xl:h-52"
+            style={{ backgroundImage: "url(MicrosoftTeams-image12.png)" }}
+          >
+            <h1 className="md:text-3xl">TROUVER UN EMPLOI</h1>
+          </div>
+          <br /> <br />
+          <div className="breadcrumb px-4 pt-4 font-semibold md:px-14 lg:px-20 xl:px-52">
+            <a href="/">Accueil</a>
+            {">"}
+            <a href="/Services">Services</a> {">"}
+            <a className="text-[#D70B52]" href="/TrouverUnEmploi">
+              Trouver un emploi
+            </a>
+          </div>{" "}
+          <br />
+          <div>
+            <div className="flex flex-col px-4 pt-4  font-semibold bg-[#F6F6F6] md:px-14 lg:px-20 xl:px-52">
+              <div className="flex-col font-sans">
+                <div className="flex-none w-full">
+                  <img
+                    src="/smileyrousse.jpg"
+                    alt=""
+                    className="object-cover"
+                    loading="lazy"
+                  />
+                </div>
+
+                <div className="bg-white">
+                  <form className="flex-auto p-6">
+                    <div className="flex flex-wrap t-20px l-20px relative">
+                      <h2 className="font-bold text-black text-lg mb-4 md:text-2xl">
+                        <span className="border-b-2 border-[#D60B52] pb-[0.5px]">
+                          LA MISSION
+                        </span>{" "}
+                        LOCALE T’ACCOMPAGNE VERS L’EMPLOI
+                      </h2>
+                      <p className="text-sm text-slate-500">
+                        Des équipes spécialisées en lien avec ton conseiller
+                        sont là pour t’aider à trouver une emploi ! Elles sont
+                        là pour négocier des offres d’emploi avec les
+                        entreprises, organiser des actions de recrutement ainsi
+                        que te préparer activement à la recherche d’emploi et à
+                        la rencontre avec les employeurs.
+                      </p>
+                      <br />
+                      <p className="text-sm text-slate-500">
+                        N’hésite donc pas à prendre rendez-vous avec ton
+                        conseiller si tu es à la recherche d’un emploi et pour
+                        que l’on puisse te présenter nos conseils et nos
+                        différents dispositifs d’insertion professionnelle.
+                      </p>
+                      <div className="flex flex-col px-4 pt-4 font-semibold md:px-14 lg:px-20">
+                        {error && <div className="text-red-500">{error}</div>}{" "}
+                        {/* Display error message */}
+                        {success && (
+                          <div className="text-green-500">{success}</div>
+                        )}{" "}
+                        {/* Display success message */}
+                        <button
+                          onClick={handleInscription}
+                          className="bg-[#D60B52] text-white font-semibold py-2 px-2 text-center rounded-lg text-sm mb-6 md:w-64"
+                        >
+                          PRENDRE RDV AVEC UN CONSEILLER
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>{" "}
+            <br />
+          </div>{" "}
+          <br />
         </div>
-        </div>
-        </div> <br />
-        </div> <br />
-        </div>
-        
+      )}
+    </>
   );
 }
 
