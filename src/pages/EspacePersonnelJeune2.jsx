c75792fd8b993f3c169866f7aa69aeec8d005884
J-Christophe Lejeune <38323623+jclejeune@users.noreply.github.com>
2024-02-12 17:27:00 +0100

    envoi et recupération de message de notification via  axios (json)

--- src/pages/EspacePersonnelJeune2.jsx
+++ src/pages/EspacePersonnelJeune2.jsx
@@ -6,10 +6,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import moment from 'moment';
 import { useAuth } from '../context/AuthContext';
-import mqtt from 'mqtt';
+// import mqtt from 'mqtt';
 
 function EspacePersonnelJeune2() {
-  
+
   const {
     register,
     handleSubmit,
@@ -22,70 +22,130 @@ function EspacePersonnelJeune2() {
       : import.meta.env.VITE_API_URL_DEV;
 
 
-  const { information, ateliers, documents, jobOffers, rendezVous , formations} = useAuth();
+
+
+  const { information, ateliers, documents, jobOffers, rendezVous, formations } = useAuth();
   const authToken = localStorage.getItem("authToken");
 
   const [oldSelectedOption, setOldSelectedOption] = useState(null);
 
-  const onSubmit = (data, e) => {
+  const onSubmit = async (data, e) => {
     const selectedOption = data.option;
     e.preventDefault();
 
     if (oldSelectedOption === selectedOption) {
       console.log("Ne spammez pas votre manager !");
     } else {
-      switch (selectedOption) {
-        case "1":
-          console.log("1");
-          axios
-            .post(`${apiUrlEnv}/api/notifierConseillerFormulaire`, {
-              interest: '1'
-            }, {
-              headers: {
-                'Authorization': `Bearer ${authToken}`,
-                'Content-Type': 'application/json'
+      try {
+        let response;
+        switch (selectedOption) {
+          case "1":
+            console.log("1");
+            response = await axios.post(
+              `${apiUrlEnv}/api/soumettre-formulaire`,
+              {
+                'jeune_id': information.id,
+                'choix': '1'
+              },
+              {
+                headers: {
+                  'Authorization': `Bearer ${authToken}`,
+                  'Content-Type': 'application/json'
+                }
+              },
+              200
+            );
+            console.log("response 1:", response.data);
+            // Gérer la réponse ici
+            if (response.data.success) {
+              // Succès
+              console.log("Formulaire soumis avec succès:", response.data.success);
+              // Faire quelque chose pour traiter le succès, si nécessaire
+            } else {
+              // Erreur
+              console.error("Erreur lors de la soumission du formulaire:", response.data.error);
+              // Faire quelque chose pour traiter l'erreur, si nécessaire
+            }
+            break;
+
+          case "2":
+            console.log("2");
+            response = await axios.post(
+              `${apiUrlEnv}/api/soumettre-formulaire`,
+              {
+                'jeune_id': information.id,
+                'choix': '2'
+              },
+              {
+                headers: {
+                  'Authorization': `Bearer ${authToken}`,
+                  'Content-Type': 'application/json'
+                }
+              },
+              200
+            );
+            console.log("response 2:", response.data);
+
+            // Gérer la réponse ici
+            if (response.data.success) {
+              // Succès
+              console.log("Formulaire soumis avec succès:", response.data.success);
+              // Faire quelque chose pour traiter le succès, si nécessaire
+            } else {
+              // Erreur
+              console.error("Erreur lors de la soumission du formulaire:", response.data.error);
+              // Faire quelque chose pour traiter l'erreur, si nécessaire
+            }
+            break;
+
+            case "3":
+              console.log("3");
+              response = await axios.post(
+                `${apiUrlEnv}/api/soumettre-formulaire`,
+                {
+                  'jeune_id': information.id,
+                  'choix': '3'
+                },
+                {
+                  headers: {
+                    'Authorization': `Bearer ${authToken}`,
+                    'Content-Type': 'application/json'
+                  }
+                },
+                200
+              );
+              console.log("response 3:", response.data);
+  
+              // Gérer la réponse ici
+              if (response.data.success) {
+                // Succès
+                console.log("Formulaire soumis avec succès:", response.data.success);
+                // Faire quelque chose pour traiter le succès, si nécessaire
+              } else {
+                // Erreur
+                console.error("Erreur lors de la soumission du formulaire:", response.data.error);
+                // Faire quelque chose pour traiter l'erreur, si nécessaire
               }
-            })
-            .then(function (response) {console.log(response);})
-            .catch(function (error) {console.log(error);});
-          break;
-        case "2":
-          console.log("2");
-          axios
-          .post(`${apiUrlEnv}/api/notifierConseillerFormulaire`, {
-            interest: '2'
-          }, {
-            headers: {
-              'Authorization': `Bearer ${authToken}`,
-              'Content-Type': 'application/json'
-            }
-          })
-            .then(function (response) {console.log(response);})
-            .catch(function (error) {console.log(error);});
-          break;
-        case "3":
-          console.log("3");
-          axios
-          .post(`${apiUrlEnv}/api/notifierConseillerFormulaire`, {
-            interest: '3'
-          }, {
-            headers: {
-              'Authorization': `Bearer ${authToken}`,
-              'Content-Type': 'application/json'
-            }
-          })
-          .then(function (response) {console.log(response);})
-          .catch(function (error) {console.log(error);});
-          break;
-        default:
-          console.log("toto comment ça se fait ?");
+              break;
+
+          default:
+            console.log("Option inconnue");
+            break;
+        }
+      } catch (error) {
+        console.error("Erreur lors de la requête Axios:", error);
+        console.log("Détails:", error.response);
+        if (error.response && error.response.status === 401) {
+          console.log("Erreur d'authentification");
+        }
       }
+
     }
 
     setOldSelectedOption(selectedOption); // Mise à jour de l'état local
   };
 
-  
+
 
   return (
     <div>
@@ -106,33 +166,33 @@ function EspacePersonnelJeune2() {
               <div className="flex flex-col ml-8">
                 <div className="mb-2">
                   <input
-                    type="radio" 
-                    id="rdvOption" 
-                    {...register("option", { required: true })} 
-                    value="1" 
+                    type="radio"
+                    id="rdvOption"
+                    {...register("option", { required: true })}
+                    value="1"
                     className="mr-2" />
                   <label htmlFor="rdvOption" className="mr-4">Avoir un RDV</label>
                 </div>
 
                 <div className="mb-2">
-                  <input 
-                    type="radio" 
+                  <input
+                    type="radio"
                     id="formationOption"
                     {...register("option", { required: true })}
-                    value="2" 
-                    className="mr-2"/>
-                    <label htmlFor="formationOption" 
+                    value="2"
+                    className="mr-2" />
+                  <label htmlFor="formationOption"
                     className="mr-2">Chercher une formation</label>
                 </div>
 
                 <div className="mb-2">
-                  <input 
-                    type="radio" 
+                  <input
+                    type="radio"
                     id="emploiOption"
                     {...register("option", { required: true })}
                     value="3"
-                    className="mr-2"/>
-                    <label htmlFor="emploiOption">Trouver un emploi</label>
+                    className="mr-2" />
+                  <label htmlFor="emploiOption">Trouver un emploi</label>
                 </div>
               </div>
             </div>
@@ -146,62 +206,62 @@ function EspacePersonnelJeune2() {
         </div>
       </div>
 
-      
+
       <h2 className="font-bold text-black text-center text-lg md:text-2xl">INFORMATIONS PERSONNELLES</h2>
       <div className="px-4 flex flex-col items-center bg-[#F6F6F6]">
         <div className="flex flex-col items-center w-[98%]">
-            <div className="flex flex-col mt-4 mb-6 p-4 w-full md:w-[70%] lg:w-[80%] bg-white rounded-3xl">
-              {/* Render the first section of personal information */}
-              <div className="absolute top-2 right-4">
-                <Link>
-                  <FaPencil />
-                </Link>
-              </div>
-              {/* <p className="font-bold mb-2">CIVILITE: </p> */}
-              <p className="mb-2">PRENOM: {information.first_name}</p>
-              <p className="mb-2">NOM: {information.last_name}</p>
-              <p className="mb-2">DATE DE NAISSANCE: {moment(information.dateOfBirth).format('DD/MM/YYYY')}</p>
-              <p className="mb-2 leading-tight">N°, TYPE, LIBELLE DE LA VOIE:</p>
-              <p className="mb-2">CODE POSTAL: </p>
-              <p className="mb-2">COMMUNE: {information.city}</p>
-              <p id="rendezvouzSection" className="mb-2">ADRESSE ELECTRONIQUE: {information.email}</p>
-              <p className="mb-2">TELEPHONE PRINCIPAL: {information.phone}</p>
+          <div className="flex flex-col mt-4 mb-6 p-4 w-full md:w-[70%] lg:w-[80%] bg-white rounded-3xl">
+            {/* Render the first section of personal information */}
+            <div className="absolute top-2 right-4">
+              <Link>
+                <FaPencil />
+              </Link>
             </div>
+            {/* <p className="font-bold mb-2">CIVILITE: </p> */}
+            <p className="mb-2">PRENOM: {information.first_name}</p>
+            <p className="mb-2">NOM: {information.last_name}</p>
+            <p className="mb-2">DATE DE NAISSANCE: {moment(information.dateOfBirth).format('DD/MM/YYYY')}</p>
+            <p className="mb-2 leading-tight">N°, TYPE, LIBELLE DE LA VOIE:</p>
+            <p className="mb-2">CODE POSTAL: </p>
+            <p className="mb-2">COMMUNE: {information.city}</p>
+            <p id="rendezvouzSection" className="mb-2">ADRESSE ELECTRONIQUE: {information.email}</p>
+            <p className="mb-2">TELEPHONE PRINCIPAL: {information.phone}</p>
+          </div>
         </div>
       </div>
       <h2 id="rendezvousSection" className="font-bold text-black text-center text-lg md:text-2xl">MES RENDEZ-VOUS</h2>
       <div className="px-4 flex flex-col items-center bg-[#F6F6F6]">
         <div className="flex flex-col items-center w-[98%]">
-            <div className="flex flex-col mt-4 mb-6 p-4 w-full md:w-[70%] lg:w-[80%] bg-white rounded-3xl">
-              {rendezVous.map((rendezVous, index) => (
-                <div key={index} className="flex flex-col text-start relative w-full">
-                  <p className="mb-2">Le {moment(rendezVous.date).format('DD/MM/YYYY')} à {moment(rendezVous.heure, 'HH:mm:ss').format('HH:mm')} avec {rendezVous.conseiller.information.first_name}</p>
-                </div>
-              ))}
-            </div>
+          <div className="flex flex-col mt-4 mb-6 p-4 w-full md:w-[70%] lg:w-[80%] bg-white rounded-3xl">
+            {rendezVous.map((rendezVous, index) => (
+              <div key={index} className="flex flex-col text-start relative w-full">
+                <p className="mb-2">Le {moment(rendezVous.date).format('DD/MM/YYYY')} à {moment(rendezVous.heure, 'HH:mm:ss').format('HH:mm')} avec {rendezVous.conseiller.information.first_name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <h2 id="docuSection" className="font-bold text-black text-center text-lg md:text-2xl">MES DOCUMENTS</h2>
       <div className="px-4 flex flex-col items-center bg-[#F6F6F6]">
-      <div className="flex flex-col items-center w-full md:w-[70%] lg:w-[80%]">
-        {documents && documents.length > 0 ? (
-          <div className="flex flex-col mt-4 mb-6 p-4 w-full bg-white rounded-3xl">
-            {documents.map((document, index) => (
-              <div key={index} className="flex justify-between items-center mb-2">
-                <p className="mb-2">{document.title}</p>
-                <button className="px-2 py-1 bg-gray-300 rounded-md">
-                  <a href={document.document_url} target="_blank" rel="noopener noreferrer">
-                    Télécharger
-                  </a>
-                </button>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Aucun document disponible.</p>
-        )}
+        <div className="flex flex-col items-center w-full md:w-[70%] lg:w-[80%]">
+          {documents && documents.length > 0 ? (
+            <div className="flex flex-col mt-4 mb-6 p-4 w-full bg-white rounded-3xl">
+              {documents.map((document, index) => (
+                <div key={index} className="flex justify-between items-center mb-2">
+                  <p className="mb-2">{document.title}</p>
+                  <button className="px-2 py-1 bg-gray-300 rounded-md">
+                    <a href={document.document_url} target="_blank" rel="noopener noreferrer">
+                      Télécharger
+                    </a>
+                  </button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>Aucun document disponible.</p>
+          )}
+        </div>
       </div>
-    </div>
 
 
       <h2 id="atelierSection" className="font-bold text-black text-center text-lg md:text-2xl">MES ATELIERS</h2>
@@ -211,7 +271,7 @@ function EspacePersonnelJeune2() {
             {ateliers && ateliers.length > 0 ? (
               ateliers.map((atelier) => (
                 <div key={atelier.id} className="flex justify-start items-center mb-2">
-                  <div  className="w-1/4"><p>Le {moment(atelier.date).format('DD/MM/YY')}</p></div>
+                  <div className="w-1/4"><p>Le {moment(atelier.date).format('DD/MM/YY')}</p></div>
                   <div className="w-1/2"><p>{atelier.title}</p></div>
                   {(() => {
                     switch (atelier.pivot.status) {
@@ -219,7 +279,7 @@ function EspacePersonnelJeune2() {
                         return <div className="w-1/4"><p className="text-xs md:text-base leading-tight text-green-500 font-semibold md:flex justify-start items-center"><FaCheckCircle className="md:ml-2" /> Inscription acceptée</p></div>;
                       case 'refuse':
                         return <div className="w-1/4"><p className="text-xs md:text-base leading-tight text-red-600 font-semibold md:flex justify-start items-center"> <FaTimesCircle className="md:ml-2" /> Inscription refusée</p></div>;
-                      default: 
+                      default:
                         return <div className="w-1/4"><p className="text-xs md:text-base leading-tight text-gray-500 font-semibold md:flex justify-start items-center"> <FaPauseCircle className="md:ml-2" /> En attente</p></div>;
                     }
                   })()}
