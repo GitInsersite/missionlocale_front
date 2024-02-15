b3df172fefc996979e8da954060bd17782462501
nitrobanko <75261692+nitrobanko@users.noreply.github.com>
2024-02-15 10:20:28 +0100

    v

--- src/pages/NosMissions.jsx
+++ src/pages/NosMissions.jsx
@@ -1,3 +1,6 @@
+import '/nosmissions.css';
+
+
 function NosMissions() {
   const handleDownload = async () => {
     try {
@@ -28,6 +31,7 @@ function NosMissions() {
         <a className="hover:text-[#D70B52]" href="/Etreaccompagne"> Nos missions</a>
       </div> 
 
+        <div className="texte" style={{ opacity: 0, animation: 'fadeIn 1s ease-in-out forwards' }}>
         <div className='flex flex-col mx-4 mt-4 md:mx-14 lg:mx-20 xl:mx-56'>
             <h1 className='font-bold text-black text-lg mb-8 md:text-2xl'><span className='border-b-2 border-[#A4195C] pb-[0.5px]'>Qui</span> sommes-nous ? </h1>
             <p className='mb-6 leading-tight '> La Mission Locale des Mureaux est un lieu d’accueil, d’orientation et d’aide pour tous les jeunes de 16 à 25 ans qui rencontrent des freins pour entrer dans la vie professionnelle. Au sein de la Mission Locale, découvrez tout ce dont vous avez besoin pour trouver un emploi, un stage, ou une alternance. Cela passe par de la formation, de l’orientation, de l’aide à la réalisation de CV, lettre de motivation, entretien d’embauche, de la mise en contact avec des employeurs, et découverte des métiers pour vous accompagner dans votre orientation professionnelle. Nous vous accompagnons aussi dans toutes les tâches administratives que vous pourrez rencontrer dans la vie active, et vous assistons pour tout problème urgent de santé ou de logement.   </p>
@@ -36,7 +40,9 @@ function NosMissions() {
             <p className='mb-6 leading-tight '>Notre objectif principal est d’accompagner tout jeune dans son entrée en vie active, en proposant des services et accompagnements personnalisés, et l’aidant ainsi à s’épanouir tout en construisant son avenir professionnel. L’avenir des jeunes, et leur insertion à la fois professionnelle et sociale, est le pilier fondateur de la Mission Locale des Mureaux, et ce pour quoi nous continuons d’agir. </p>
            
             <h1 className='font-bold text-black text-lg mb-8 md:text-2xl'> <span className="border-b-2 border-[#A4195C] pb-[0.5px]">Dis</span>positifs</h1>
+            
             <button className="bg-[#A4195C] text-white font-semibold py-1 text-center rounded-lg mb-8 text-sm md:w-56 hover:bg-pink-500 transition-all duration-300" onClick={handleDownload}>TELECHARGER NOTRE GUIDE</button>
+            </div>
         </div>
     </div>
   );
