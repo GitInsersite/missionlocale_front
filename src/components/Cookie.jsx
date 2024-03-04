28b5ce2d03bb1aa26c1b6a95c73ced8b143bd6a9
nitrobanko <75261692+nitrobanko@users.noreply.github.com>
2024-03-04 16:49:05 +0100

    cookv1

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import CookieConsent from "react-cookie-consent";
 import "/CookieBanner.css"; 
 
+
 function Cookie() {
   return (
     <CookieConsent
+      containerClasses="cookie-container"
+      buttonClasses=""
+      declineButtonClasses=""
       location="bottom"
       buttonText="Accepter"
       cookieName="myAwesomeCookieName2"
@@ -16,7 +20,11 @@ function Cookie() {
         alert("Pour accéder au site, vous devez accepter l'utilisation de cookies.");
       }}
     >
-      Ce site utilise des cookies pour améliorer votre expérience. En continuant à naviguer sur le site, vous acceptez l'utilisation de cookies.
+      <div className="cookie-content">
+        <img src="/public/cooks.png" alt="Cookie image" className="cookie-image"/>
+        <p>      Ce site utilise des cookies pour améliorer votre expérience. En continuant à naviguer sur le site, vous acceptez l'utilisation de cookies.
+</p>
+      </div>
     </CookieConsent>
   );
 }
